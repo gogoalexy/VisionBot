561f0a3255897b81d8e180a77be5732fde139fef
Alex Yao <cktfg18nthu@gapp.nthu.edu.tw>
2019-08-27 20:34:38 +0800

    The optic flow evaluation tool is based on S. Baker et al.(2011)

--- eval/of_eval.py
+++ eval/of_eval.py
@@ -1,5 +1,5 @@
 import numpy as np
-    
+
 def calcAngularError(predictedFlow, groundtruthFlow):
     numerator = 1.0 + predictedFlow[0]*groundtruthFlow[0] + predictedFlow[1]*groundtruthFlow[1]
     denominator = np.sqrt(1.0 + predictedFlow[0]**2 + predictedFlow[1]**2) * np.sqrt(1.0 + groundtruthFlow[0]**2 + groundtruthFlow[1]**2)
@@ -26,8 +26,13 @@ def getFlowErrorAverage(errorsInOneFrame):
 def getFlowErrorStandardDeviation(errorsInOneFrame):
     return np.std(errorsInOneFrame)
 
-def sortFromSmall2Large(errorsInOneFrame):
+def sortAscending(errorsInOneFrame):
     return np.sort(errorsInOneFrame)
 
 def getPercentageOfErrorOverThreshold(sortedErrorsInOneFrame, threshold):
-    
+    numAllPixels = sortedErrorsInOneFrame.size()
+    numPixelsWithErrorLargerThanThreshold = np.searchsorted(sortedErrorsInOneFrame, threshold, side='left')
+    return (numPixelsWithErrorLargerThanThreshold / numAllPixels) * 100.0
+
+def getErrorAtNthPercentiles(errorsInOneFrame, nthPercentiles):
+    return np.percentile(errorsInOneFrame, nthPercentiles)
