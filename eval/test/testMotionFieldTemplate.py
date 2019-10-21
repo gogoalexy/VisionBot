6ff06fc6d86d214c9ecac2843e36c8fb5c23785b
Alex Yao <cktfg18nthu@gapp.nthu.edu.tw>
2019-10-21 15:13:38 +0800

    Draw motion field in a more reasonable way.

--- eval/test/testMotionFieldTemplate.py
+++ eval/test/testMotionFieldTemplate.py
@@ -1,119 +1,90 @@
+import sys
+
 import matplotlib.pyplot as plt
 import numpy as np
-import sys
+
 sys.path.append("../")
 import motionFieldTemplate as mft
 
-left = mft.createCamLeftShiftTemplate(8, 8)
-U = np.empty((8, 8))
-for y in range(0, 8):
-    for x in range(0, 8):
-        U[y][x] = left[y][x][0]
-V = np.empty((8, 8))
-for y in range(0, 8):
-    for x in range(0, 8):
-        V[y][x] = left[y][x][1]
+def traverseSplitXY(field):
+    U = np.empty((field.shape[0], field.shape[1]))
+    for y in range(0, field.shape[0]):
+        for x in range(0, field.shape[1]):
+            U[y][x] = field[y][x][0]
+    V = np.empty((field.shape[0], field.shape[1]))
+    for y in range(0, field.shape[0]):
+        for x in range(0, field.shape[1]):
+            V[y][x] = field[y][x][1]
+    return U, V
 
-plt.figure(0)
+templatex = 16
+templatey = 16
+
+plt.figure(0, figsize=(6, 6))
+left = mft.createCamLeftShiftTemplate(templatex, templatey)
+U, V = traverseSplitXY(left)
+
+plt.subplot(2, 2, 1)
 plt.title("Camera Pan Left")
 plt.quiver(U, V)
 
-right = mft.createCamRightShiftTemplate(8, 8)
-U = np.empty((8, 8))
-for y in range(0, 8):
-    for x in range(0, 8):
-        U[y][x] = right[y][x][0]
-V = np.empty((8, 8))
-for y in range(0, 8):
-    for x in range(0, 8):
-        V[y][x] = right[y][x][1]
 
-plt.figure(1)
+right = mft.createCamRightShiftTemplate(templatex, templatey)
+U, V = traverseSplitXY(right)
+
+plt.subplot(2, 2, 2)
 plt.title("Camera Pan Right")
 plt.quiver(U, V)
 
-up = mft.createCamUpShiftTemplate(8, 8)
-U = np.empty((8, 8))
-for y in range(0, 8):
-    for x in range(0, 8):
-        U[y][x] = up[y][x][0]
-V = np.empty((8, 8))
-for y in range(0, 8):
-    for x in range(0, 8):
-        V[y][x] = up[y][x][1]
 
-plt.figure(2)
+up = mft.createCamUpShiftTemplate(templatex, templatey)
+U, V = traverseSplitXY(up)
+
+plt.subplot(2, 2, 3)
 plt.title("Camera Pan Up")
 plt.quiver(U, V)
 
-down = mft.createCamDownShiftTemplate(8, 8)
-U = np.empty((8, 8))
-for y in range(0, 8):
-    for x in range(0, 8):
-        U[y][x] = down[y][x][0]
-V = np.empty((8, 8))
-for y in range(0, 8):
-    for x in range(0, 8):
-        V[y][x] = down[y][x][1]
 
-plt.figure(3)
+down = mft.createCamDownShiftTemplate(templatex, templatey)
+U, V = traverseSplitXY(down)
+
+plt.subplot(2, 2, 4)
 plt.title("Camera Pan Down")
 plt.quiver(U, V)
+plt.tight_layout()
 
-zoomin = mft.createCamZoomInTemplate(8, 8)
-U = np.empty((8, 8))
-for y in range(0, 8):
-    for x in range(0, 8):
-        U[y][x] = zoomin[y][x][0]
-V = np.empty((8, 8))
-for y in range(0, 8):
-    for x in range(0, 8):
-        V[y][x] = zoomin[y][x][1]
 
-plt.figure(4)
+plt.figure(1, figsize=(6, 6))
+zoomin = mft.createCamZoomInTemplate(templatex, templatey)
+U, V = traverseSplitXY(zoomin)
+
+plt.subplot(2, 2, 1)
 plt.title("Camera Zoom In")
 plt.quiver(U, V)
 
-zoomout = mft.createCamZoomOutTemplate(8, 8)
-U = np.empty((8, 8))
-for y in range(0, 8):
-    for x in range(0, 8):
-        U[y][x] = zoomout[y][x][0]
-V = np.empty((8, 8))
-for y in range(0, 8):
-    for x in range(0, 8):
-        V[y][x] = zoomout[y][x][1]
 
-plt.figure(5)
+zoomout = mft.createCamZoomOutTemplate(templatex, templatey)
+U, V = traverseSplitXY(zoomout)
+
+plt.subplot(2, 2, 2)
 plt.title("Camera Zoom Out")
 plt.quiver(U, V)
 
-rotatecw = mft.createCamRotateCWTemplate(8, 8)
-U = np.empty((8, 8))
-for y in range(0, 8):
-    for x in range(0, 8):
-        U[y][x] = rotatecw[y][x][0]
-V = np.empty((8, 8))
-for y in range(0, 8):
-    for x in range(0, 8):
-        V[y][x] = rotatecw[y][x][1]
 
-plt.figure(6)
+rotatecw = mft.createCamRotateCWTemplate(templatex, templatey)
+U, V = traverseSplitXY(rotatecw)
+
+plt.subplot(2, 2, 3)
 plt.title("Camera Rotate CW")
 plt.quiver(U, V)
 
-rotateccw = mft.createCamRotateCCWTemplate(8, 8)
-U = np.empty((8, 8))
-for y in range(0, 8):
-    for x in range(0, 8):
-        U[y][x] = rotateccw[y][x][0]
-V = np.empty((8, 8))
-for y in range(0, 8):
-    for x in range(0, 8):
-        V[y][x] = rotateccw[y][x][1]
 
-plt.figure(7)
+rotateccw = mft.createCamRotateCCWTemplate(templatex, templatey)
+U, V = traverseSplitXY(rotateccw)
+
+plt.subplot(2, 2, 4)
 plt.title("Camera Rotate CCW")
 plt.quiver(U, V)
+plt.tight_layout()
 
 plt.show()
