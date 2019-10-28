9574ef8975a68248835815aef7479edfc3e36ea4
Alex Yao <cktfg18nthu@gapp.nthu.edu.tw>
2019-10-28 15:24:13 +0800

    True graphical representation of neural activity.

--- eval/main.py
+++ eval/main.py
@@ -14,9 +14,10 @@ ap = argparse.ArgumentParser()
 framesToLoop = ap.add_mutually_exclusive_group()
 framesToLoop.add_argument("-n", "--num-frames", type=int, default=1000, help="# of frames to loop over for FPS test")
 framesToLoop.add_argument("-c", "--continuous", help="Set number of frames to loop over to infinity", action="store_true")
-ap.add_argument("-s", "--steps", type=int, default=500, help="# of steps to simulate for each frame")
+ap.add_argument("-s", "--steps", type=int, default=50, help="# of steps to simulate for each frame")
 ap.add_argument("-t", "--num-threads", type=int, default=1, help="# of threads to accelerate")
-ap.add_argument("-d", "--display", help="Whether or not frames should be displayed", action="store_true")
+ap.add_argument("-dn", "--display-neuron", help="Whether or not neural activities should be displayed", action="store_true")
+ap.add_argument("-dt", "--display-dot", help="Whether or not dotted results should be displayed", action="store_true")
 ap.add_argument("-df", "--display-flow", help="Whether or not flow frames should be displayed", action="store_true")
 ap.add_argument("-p", "--picamera", help="Whether or not the Raspberry Pi camera should be used", action="store_true")
 ap.add_argument("-iz", "--izhikevich", help="Use Izhikevich neuron model instead of IQIF", action="store_true")
@@ -39,7 +40,10 @@ while True:
     FlattenFlow = algo.calculateOpticalFlow(prvs, curr).flatten()
     dottedFlow = motionFieldTemplate.dotWithTemplates(FlattenFlow, AllFlattenTemplates)
     normalizedDottedFlow = [ int(dotted//500) for dotted in dottedFlow ]
-
+    snn.stimulateInOrder(normalizedDottedFlow)
+    snn.run(args["steps"])
+    activity = snn.getAllActivityInOrder()
+    
     if args["display_flow"]:
         showFrame = curr.copy()
         interval = 8
@@ -50,20 +54,25 @@ while True:
         cv2.imshow("Flow", showFrame)
         cv2.waitKey(1)
 
-    if args["display"]:
+    if args["display_dot"]:
         showFrame = cv2.resize(cv2.cvtColor(curr, cv2.COLOR_GRAY2BGR), (512, 512))
         interval = 60
         cv2.putText(showFrame, "UP   DOWN  LEFT  RIGHT   IN     OUT    CW    CCW", (10, 330), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255))
         for loc, val in enumerate(normalizedDottedFlow):
             cv2.line(showFrame, (40+loc*interval, 300), (40+loc*interval, 300-val*10), color=(255, 255, 255), thickness=20)
-        
         cv2.imshow("Dotted", showFrame)
         cv2.waitKey(1)
 
+    if args["display_neuron"]:
+        showFrame = cv2.resize(cv2.cvtColor(curr, cv2.COLOR_GRAY2BGR), (512, 512))
+        interval = 60
+        cv2.putText(showFrame, "UP   DOWN  LEFT  RIGHT   IN     OUT    CW    CCW", (10, 450), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255))
+        for loc, val in enumerate(activity):
+            cv2.line(showFrame, (40+loc*interval, 450), (40+loc*interval, 450-val*10), color=(255, 255, 55), thickness=20)
+        cv2.imshow("Neuron", showFrame)
+        cv2.waitKey(1)
+
     prvs = curr
-    snn.stimulateInOrder(normalizedDottedFlow)
-    snn.run(args["steps"])
-    activity = snn.getAllActivityInOrder()
     led.turnOffAll()
     led.turnOnConfig(5, activity)
     fps.update()
