aa7afb3d8dba4f6d64cc270215c7c50416d13cc7
Alex Yao <cktfg18nthu@gapp.nthu.edu.tw>
2019-11-16 18:45:29 +0800

    Clean up OpenMV stuff.

--- Dataset/Recorder/picam_mjpeg.py
+++
@@ -1,7 +0,0 @@
-import picamera
-
-camera = picamera.PiCamera()
-camera.resolution = (320, 240)
-camera.start_recording('my_video.mjpg', format=mjpeg)
-camera.wait_recording(15)
-camera.stop_recording()
