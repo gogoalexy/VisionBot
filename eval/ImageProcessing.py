44540fba328f232a0b482629c0e223e120482a7d
twetto <franky85@hotmail.com.tw>
2020-02-11 16:10:15 +0800

    Fix typos

--- eval/ImageProcessing.py
+++ eval/ImageProcessing.py
@@ -60,6 +60,6 @@ class Algorithm:
         # GaussianKernalSize1 = 7
         # GaussianSTD1 = 5.0
         blur = cv2.GaussianBlur(frame, (7, 7), 5.0)
-        unsharpFrame = cv2.addWeighted(frame, t, blur, -4, 0, frame)
+        unsharpFrame = cv2.addWeighted(frame, 5, blur, -4, 0, frame)
         return unsharpFrame
 
