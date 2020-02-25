34b11f7e4e39c356660b24c20117ff5bbb0d1dd4
Alex Yao <huangyu_yao@lolab-nthu.org>
2020-02-25 15:53:04 +0800

    Restore rotation movement indicator. (commit 5a92fcb259)

--- eval/Graphics.py
+++ eval/Graphics.py
@@ -5,14 +5,14 @@ class Graphics():
 
 
     def __init__(self):
-        upArrow = np.array([ ( (87, 300), (87, 50) ), ( (174, 300), (174, 50) ), ( (261, 300), (261, 50) ) ])
-        downArrow = np.array([ ( (87, 400), (87, 650) ), ( (174, 400), (174, 650) ), ( (261, 400), (261, 650) ) ])
-        leftArrow = np.array([ ( (1000, 437), (750, 437) ), ( (1000, 524), (750, 524) ), ( (1000, 611), (750, 611) ) ])
-        rightArrow = np.array([ ( (750, 87), (1000, 87) ), ( (750, 174), (1000, 174) ), ( (750, 261), (1000, 261) ) ])
-        zoominArrow = np.array([ ( (500, 150), (400, 50) ), ( (500, 200), (400, 300) ), ( (550, 150), (650, 50) ), ( (550, 200), (650, 300) ), ( (500, 175), (375, 175) ), ( (550, 175), (675, 175) ), ( (525, 150), (525, 25) ), ( (525, 200), (525, 325) ) ])
-        zoomoutArrow = np.array([ ( (400, 400), (475, 475) ), ( (400, 650), (475, 575) ), ( (650, 400), (575, 475) ), ( (650, 650), (575, 575) ), ( (375, 525), (475, 525) ), ( (675, 525), (575, 525) ), ( (525, 375), (525, 475) ), ( (525, 675), (525, 575) ) ])
-        cwArrow = np.array( [  ] )
-        ccwArrow = np.array( [  ] )
+        upArrow = np.array([ ( (270, 450), (270, 250) ), ( (350, 450), (350, 250) ), ( (430, 450), (430, 250) ) ])
+        downArrow = np.array([ ( (270, 250), (270, 450) ), ( (350, 250), (350, 450) ), ( (430, 250), (430, 450) ) ])
+        rightArrow = np.array([ ( (550, 270), (750, 270) ), ( (550, 350), (750, 350) ), ( (550, 430), (750, 430) ) ])
+        leftArrow = np.array([ ( (750, 270), (550, 270) ), ( (750, 350), (550, 350) ), ( (750, 430), (550, 430) ) ])
+        zoominArrow = np.array([ ( (330, 630), (230, 530) ), ((330, 670), (230, 770)  ), ( (370, 630), (470, 530) ), ((370, 670), (470, 770)  ) ])
+        zoomoutArrow = np.array([ ((230, 530), (330, 630)  ), ( (230, 770), (330, 670) ), ( (470, 530), (370, 630) ), ((470, 770), (370, 670)  ) ])
+        cwArrow = np.array( [ (0, 0), (0, 0) ] )
+        ccwArrow = np.array( [ (0, 0), (0, 0) ] )
         frontSide = np.array([ ( (0, 0), (249, 50) ) ])
         rearSide = np.array([ ( (249, 249), (0, 200) ) ])
         leftSide = np.array([ ( (0, 50), (50, 200) ) ])
@@ -34,7 +34,7 @@ class Graphics():
         for index in range(positions.shape[0]):
             cv2.arrowedLine(image, tuple(positions[index][0]), tuple(positions[index][1]), color, thickness=20)
         return image
-    
+
     def drawObstacles(self, image, locations, color):
         for index in range(locations.shape[0]):
             cv2.rectangle(image, tuple(locations[index][0]), tuple(locations[index][1]), color, thickness=cv2.FILLED)
@@ -43,14 +43,26 @@ class Graphics():
     # front, rear, left, right
     def displayConfig(self, thresholds, config):
         motion = self.canvas.copy()
-        #obstacle = self.blank.copy()
-        for index in range(8):
-            if config[index] > thresholds[0]:
-                motion = self.drawArrows(motion, self.arrows[index], self.activeColor)
-        #for index in range(8, 12):
-        #    if config[index] > thresholds[1]:
-        #        obstacle = self.drawObstacles(obstacle, self.arrows[index], self.activeColor)
-        
+
+        for index, value  in enumerate(config):
+            if index == 0:
+                if value > thresholds[0]:
+                    cv2.drawMarker(motion, (550, 650), self.activeColor, cv2.MARKER_TRIANGLE_UP, markerSize=50, thickness=20)
+                    cv2.drawMarker(motion, (750, 650), self.activeColor, cv2.MARKER_TRIANGLE_DOWN, markerSize=50, thickness=20)
+            elif index == 4:
+                if value > thresholds[0]:
+                    cv2.drawMarker(motion, (550, 650), self.activeColor, cv2.MARKER_TRIANGLE_DOWN, markerSize=50, thickness=20)
+                    cv2.drawMarker(motion, (750, 650), self.activeColor, cv2.MARKER_TRIANGLE_UP, markerSize=50, thickness=20)
+            elif index < 8:
+                if value > thresholds[0]:
+                    motion = self.drawArrows(motion, self.arrows[index], self.activeColor)
+
+        if config[0] > thresholds[0] or config[4] > thresholds[0]:
+            cv2.circle(motion, (650, 650), 100, self.activeColor, thickness=20)
+
+        for index in range(8, 12):
+            if config[index] > thresholds[1]:
+                cv2.fillConvexPoly(motion, self.arrows[index], self.obstacleColor)
+
         cv2.imshow("Motion", motion)
-        #cv2.imshow("Obstacle", obstacle)
         cv2.waitKey(10)
